cc95796485d75f9da1a521a48a68efe0af52081e
Gian-Reto Tarnutzer <gian@cloudpages.ch>
2024-06-23 03:52:37 +0200

    Update tlp.nix

--- modules/features/tlp.nix
+++ modules/features/tlp.nix
@@ -14,6 +14,24 @@
   };
 
   config.os = lib.mkIf config.features.tlp.enable {
-    services.tlp.enable = true;
+    services.power-profiles-daemon.enable = false;
+    powerManagement.powertop.enable = true;
+    services.tlp = {
+      enable = true;
+
+      # See: https://github.com/LunNova/nixos-configs/blob/dev/hosts/amayadori/default.nix.
+      settings = {
+        PCIE_ASPM_ON_AC = "performance";
+        PCIE_ASPM_ON_BAT = "powersupersave";
+        RUNTIME_PM_ON_AC = "auto";
+        # Operation mode when no power supply can be detected: AC, BAT.
+        TLP_DEFAULT_MODE = "BAT";
+        # Operation mode select: 0=depend on power source, 1=always use TLP_DEFAULT_MODE.
+        TLP_PERSISTENT_DEFAULT = "1";
+        DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi wwan";
+        DEVICES_TO_DISABLE_ON_WIFI_CONNECT = "wwan";
+        DEVICES_TO_DISABLE_ON_WWAN_CONNECT = "wifi";
+      };
+    };
   };
 }
