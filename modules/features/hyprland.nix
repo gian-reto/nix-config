6820c768d48fcd8ae1757d0f8ebf4dccda3f0ac8
Gian-Reto Tarnutzer <gian@cloudpages.ch>
2024-06-30 17:21:20 +0200

    Enable `hyprland-hyprspace`

--- modules/features/hyprland.nix
+++ modules/features/hyprland.nix
@@ -86,6 +86,10 @@ in {
 
       package = pkgs.hyprland;
 
+      plugins = [
+        inputs.hyprland-hyprspace.packages.${pkgs.system}.Hyprspace
+      ];
+
       settings = let
         active = "0x66585E6A";
         inactive = "0x66434852";
