471b4288e450d7e3efb6135f01d60d8de35e6a4b
Gian-Reto Tarnutzer <hi@giantarnutzer.com>
2025-08-18 00:46:59 +0200

    Remove unused AGS v2 config

--- modules/features/ags/config/util/jsx.ts
+++
@@ -1,3 +0,0 @@
-import { Fragment } from "astal/gtk4/jsx-runtime";
-
-export type PropsWithChildren<TProps> = TProps & Parameters<typeof Fragment>[0];
--- modules/features/ags/config/util/type.ts
+++
@@ -1,4 +0,0 @@
-export type MapKey<TMap> = TMap extends Map<infer TKey, any> ? TKey : never;
-export type MapValue<TMap> = TMap extends Map<any, infer TValue>
-  ? TValue
-  : never;
