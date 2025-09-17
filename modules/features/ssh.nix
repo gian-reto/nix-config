54317386c518ba07deda300d4ef68fbb7db73698
Gian-Reto Tarnutzer <hi@giantarnutzer.com>
2025-09-17 23:47:55 +0200

    Remove deprecated `programs.ssh.enableDefaultConfig`

--- modules/features/ssh.nix
+++ modules/features/ssh.nix
@@ -20,6 +20,20 @@ in {
   config.hm = lib.mkIf config.features.ssh.enable {
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
+
+      matchBlocks."*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
 
       extraConfig = ''
         Host github.com
