0e3c3a36f40d2a1c2f5efaef91c55a38339db931
Gian-Reto Tarnutzer <hi@giantarnutzer.com>
2026-03-05 00:23:13 +0100

    Fix shutdown behavior

--- modules/features/desktop/shell.nix
+++ modules/features/desktop/shell.nix
@@ -1,7 +1,9 @@
 {
   config,
+  hmConfig,
   inputs,
   lib,
+  osConfig,
   pkgs,
   ...
 }: let
@@ -9,7 +11,23 @@
   adwShellPkg = inputs.adw-shell.packages.${pkgs.stdenv.hostPlatform.system}.default;
   agsPkg = inputs.adw-shell.packages.${pkgs.stdenv.hostPlatform.system}.ags;
   hyprshutdownPkg = inputs.hyprland-hyprshutdown.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  hyprshutdown = lib.getExe hyprshutdownPkg;
 in {
+  config.os = lib.mkIf cfg.enable {
+    # Allow the desktop user to shut down and reboot without a password prompt.
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if (
+          (action.id === "org.freedesktop.login1.power-off" ||
+           action.id === "org.freedesktop.login1.reboot") &&
+          subject.user === "${hmConfig.home.username}"
+        ) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+  };
+
   config.hm = lib.mkIf cfg.enable {
     home.packages = [
       adwShellPkg
@@ -19,13 +37,12 @@ in {
 
     xdg.configFile."adw-shell/config.json".text = let
       grimblast = lib.getExe inputs.hyprland-contrib.packages.${pkgs.stdenv.hostPlatform.system}.grimblast;
-      hyprshutdown = lib.getExe hyprshutdownPkg;
     in
       builtins.toJSON {
         commands = {
           lock = ["${pkgs.systemd}/bin/loginctl" "lock-session"];
-          "log-out" = ["${hyprshutdown}"];
-          restart = ["${hyprshutdown}" "-t" "Restarting..." "--post-cmd" "reboot"];
+          "log-out" = ["${pkgs.systemd}/bin/systemctl" "--user" "start" "desktop-logout.service"];
+          restart = ["${pkgs.systemd}/bin/systemctl" "--user" "start" "desktop-reboot.service"];
           screenshot = [
             "${grimblast}"
             "--notify"
@@ -33,28 +50,79 @@ in {
             "copy"
             "area"
           ];
-          shutdown = ["${hyprshutdown}" "-t" "Shutting down..." "--post-cmd" "shutdown -P 0"];
+          shutdown = ["${pkgs.systemd}/bin/systemctl" "--user" "start" "desktop-shutdown.service"];
           suspend = ["${pkgs.systemd}/bin/systemctl" "suspend"];
         };
       };
 
-    systemd.user.services.adw-shell = {
-      Unit = {
-        Description = "Adwaita Shell";
-        Documentation = "https://github.com/gian-reto/adw-shell";
-        PartOf = ["graphical-session.target"];
-        After = ["graphical-session-pre.target"];
+    systemd.user.services = {
+      adw-shell = {
+        Unit = {
+          Description = "Adwaita Shell";
+          Documentation = "https://github.com/gian-reto/adw-shell";
+          PartOf = ["graphical-session.target"];
+          After = ["graphical-session-pre.target"];
+        };
+
+        Service = {
+          ExecStart = "${adwShellPkg}/bin/adw-shell";
+          Restart = "on-failure";
+          RestartPreventExitStatus = 143;
+          RestartSec = 5;
+          KillMode = "mixed";
+        };
+
+        Install = {
+          WantedBy = ["graphical-session.target"];
+        };
       };
 
-      Service = {
-        ExecStart = "${adwShellPkg}/bin/adw-shell";
-        Restart = "on-failure";
-        RestartSec = 5;
-        KillMode = "mixed";
+      desktop-logout = let
+        logoutScript = pkgs.writeShellScript "desktop-logout" ''
+          ${hyprshutdown} -t "Logging out..." --post-cmd "${lib.getExe osConfig.programs.uwsm.package} stop"
+        '';
+      in {
+        Unit = {
+          Description = "Desktop-initiated session logout";
+        };
+
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${logoutScript}";
+          KillMode = "none";
+        };
       };
 
-      Install = {
-        WantedBy = ["graphical-session.target"];
+      desktop-reboot = let
+        rebootScript = pkgs.writeShellScript "desktop-reboot" ''
+          ${hyprshutdown} -t "Restarting..." --post-cmd "${pkgs.systemd}/bin/systemctl reboot"
+        '';
+      in {
+        Unit = {
+          Description = "Desktop-initiated system reboot";
+        };
+
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${rebootScript}";
+          KillMode = "none";
+        };
+      };
+
+      desktop-shutdown = let
+        shutdownScript = pkgs.writeShellScript "desktop-shutdown" ''
+          ${hyprshutdown} -t "Shutting down..." --post-cmd "${pkgs.systemd}/bin/systemctl poweroff"
+        '';
+      in {
+        Unit = {
+          Description = "Desktop-initiated system shutdown";
+        };
+
+        Service = {
+          Type = "oneshot";
+          ExecStart = "${shutdownScript}";
+          KillMode = "none";
+        };
       };
     };
   };
