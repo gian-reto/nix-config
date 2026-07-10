cb676d5d91c936198b05c0e2550540d1a9ff1272
Gian-Reto Tarnutzer <hi@giantarnutzer.com>
2026-07-10 13:01:11 +0200

    Update flake.lock

--- modules/features/desktop/cursor.nix
+++ modules/features/desktop/cursor.nix
@@ -6,10 +6,12 @@
 }:
 lib.mkIf config.features.desktop.enable {
   hm.home.pointerCursor = {
+    enable = true;
+
+    gtk.enable = true;
     name = "macOS";
     package = pkgs.apple-cursor;
     size = 24;
-    gtk.enable = true;
     x11 = {
       enable = true;
 
